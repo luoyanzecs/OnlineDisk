2ed17e1b34ee826256feb72f6d68675d4c60a696
luoyanzecs <1421286905@qq.com>
2020-08-04 04:35:56 +0800

    添加了未加密的操作

--- src/cn/team/onlinedisk/web/servlet/UploadFileServlet.java
+++ src/cn/team/onlinedisk/web/servlet/UploadFileServlet.java
@@ -12,8 +12,7 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 @MultipartConfig
 @WebServlet("/upload")
@@ -27,14 +26,14 @@ public class UploadFileServlet extends HttpServlet {
             HttpSession session = request.getSession();
             User user = (User) session.getAttribute("user_msg");
             UserFileInfoService ufiService = new UserFileInfoServiceImpl();
-
             UserFileInfo ufInfo = new UserFileInfo(user, filename);
             ufiService.saveFile(ufInfo);
             String path = ufiService.getAvailPath(ufInfo);
             part.write(path);
-            //如果想只用文件上传的功能的话把这个注释打开并且将上面一行注释掉即可
-            //String tempPath = CacheNewUtils.DIR_PATH + File.separator + filename;
-            //part.write(tempPath);
+            //如果想只用文件上传的功能的话把这个注释打开
+            //String goalPath = CacheNewUtils.DIR_PATH + File.separator + filename;
+            //this.copyFile(path, goalPath);
+
             response.getWriter().write("上传成功");
         }else {
             response.getWriter().write("上传失败");
@@ -49,6 +48,20 @@ public class UploadFileServlet extends HttpServlet {
     private boolean isFileExist(String absPath){
         File file = new File(absPath);
         return file.exists();
+    }
+
+    private void copyFile(String path, String goalPath) throws IOException {
+        File file1 = new File(path);
+        FileInputStream fis = new FileInputStream(file1);
+        File file2 = new File(goalPath);
+        FileOutputStream fos = new FileOutputStream(file2);
+        byte[] bytes = new byte[8192];
+        int len = 0;
+        while ((len = fis.read()) != -1){
+            fos.write(bytes, 0, len);
+        }
+        fis.close();
+        fos.close();
     }
 
 
